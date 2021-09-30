84cbdabc17ad853c34b453ca55cbbc19149cf358
Payman Bayat <payman1983@gmail.com>
2021-09-30 13:57:46 -0700

    Initial Commit

---
+++ SourceControl-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourceControl-Test
+//
+//  Created by Payman Bayat on 9/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SourceControl-Test/SourceControl_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControl_TestApp.swift
+//  SourceControl-Test
+//
+//  Created by Payman Bayat on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SourceControl_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
